a9e7b4e34b7bff99a03ad10241dd125bac10825a
Fiffers <14336807+Fiffers@users.noreply.github.com>
2026-02-05 09:17:51 -0800

    Update NewMessageBannerRenderer.java

--- src/main/java/com/customemoji/renderer/NewMessageBannerRenderer.java
+++ src/main/java/com/customemoji/renderer/NewMessageBannerRenderer.java
@@ -301,7 +301,7 @@ public class NewMessageBannerRenderer extends Overlay
 
 	public void onNewMessage()
 	{
-		log.info("onNewMessage called, scrolledUpPixels={}", this.chatScrollingManager.getScrolledUpPixels());
+		log.debug("onNewMessage called, scrolledUpPixels={}", this.chatScrollingManager.getScrolledUpPixels());
 		if (this.chatScrollingManager.getScrolledUpPixels() > 0)
 		{
 			this.hasNewMessageWhileScrolledUp = true;
